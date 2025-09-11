ade9d2908833016dfc2282a03caf07203739feae
Luis Sena <luis.senarode@gmail.com>
2025-09-11 12:15:18 -0300

    feat(database): configure typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { Product } from './typeorm/entities/product.entity';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [Product],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    ProductsModule,
+  ],
   controllers: [],
   providers: [],
 })
